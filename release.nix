ed31d2482fedbf26e057d23ea0e69d753097a0aa
Joel Taylor <me@joelt.io>
2014-07-30 15:15:10 -0700

    remove src

--- release.nix
+++ release.nix
@@ -1,5 +1,4 @@
-{ src ? { outPath = ./.; revCount = 0; gitTag = "dirty"; }
-, supportedPlatforms ? [ "x86_64-linux" "x86_64-darwin" ]
+{ supportedPlatforms ? [ "x86_64-linux" "x86_64-darwin" ]
 , supportedCompilers ? ["ghc6104" "ghc6123" "ghc704" "ghc722" "ghc742" "ghc763" "ghc783" "ghcHEAD"]
 }:
 
